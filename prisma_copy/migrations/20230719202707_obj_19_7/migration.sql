1d7f4a64a6f4dd5ca3e1a05758287787aeba19cd
aymen-agrebi <aymen.agrebi.dev@gmail.com>
2024-02-20 17:56:06 +0100

    correct

--- prisma_copy/migrations/20230719202707_obj_19_7/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `objectif` ADD COLUMN `current` BOOLEAN NOT NULL DEFAULT false;
--- prisma_copy/migrations/20230809051038_t9_8/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `dashbordprefrences` ADD COLUMN `rangeDate` JSON NULL;
